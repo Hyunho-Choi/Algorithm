91cd121637bb4843840dfc50139358f6ba815ce2
Hyunho-Choi <56762168+Hyunho-Choi@users.noreply.github.com>
2021-01-24 15:19:23 +0900

    Create 문제_2920.py

---
+++ 백준/문제_2920.py
@@ -0,0 +1,9 @@
+up = "1 2 3 4 5 6 7 8"
+down = "8 7 6 5 4 3 2 1"
+answer = input()
+if(answer == up):
+    print("ascending")
+elif(answer == down):
+    print("descending")
+else:
+    print("mixed")
